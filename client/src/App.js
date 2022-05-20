66eff8f127ca09d8ac6867dcc7f9b883edd800bc
Marc Cavazos <marcavazos19@icloud.com>
2022-05-19 21:14:18 -0500

    alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -19,6 +20,7 @@ const App = () => (
           <Landing />
         </Route>
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register">
               <Register />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-export const Register = () => {
+export const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -16,7 +19,7 @@ export const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
             console.log('SUCCESS');
         }
@@ -79,4 +82,12 @@ export const Register = () => {
   )
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+
+export default connect(
+  null, 
+  { setAlert }
+  )
+  (Register);
